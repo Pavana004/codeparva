9f8c70d394554bab8665a498facc8df47078a760
Pavan <101943293+Pavana004@users.noreply.github.com>
2022-12-19 13:31:46 +0530

    source

---
+++ src/container/Pettable.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import data from "../container/Petdata.json";
+
+const Pettable = () => {
+
+
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Breed</th>
+            <th scope="col">Age(month)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map(({ id, name, age, breed }) => {
+              return (
+                <tr key={id}>
+                  <td>{name}</td>
+                  <td>{breed}</td>
+                  <td>{age}</td>
+                </tr>
+              )
+            })
+          }
+
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Pettable
