f5081c296d653b2f34f09d97e4929b64f81ae481
Pavan <101943293+Pavana004@users.noreply.github.com>
2023-01-13 17:09:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router,Route} from "react-router-dom";
 import Home from "./container/Home";
-import "../src/app.css";
+import "../src/App.css";
 
 
 
